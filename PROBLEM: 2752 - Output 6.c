0338cd5560983a343e66ced73ec0656466d1ed42
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2024-06-18 23:37:14 +0600

    Create PROBLEM: 2752 - Output 6.c

---
+++ PROBLEM: 2752 - Output 6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    char a[50]="AMO FAZER EXERCICIO NO URI";
+    printf("<%s>\n<%30s>\n<%.20s>\n<%-20s>\n<%-30s>\n<%.30s>\n<%30.20s>\n<%-30.20s>\n",a,a,a,a,a,a,a,a);
+    return 0;
+}
