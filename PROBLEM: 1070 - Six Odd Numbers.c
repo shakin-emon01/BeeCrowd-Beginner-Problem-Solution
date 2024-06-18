37097525c4523a84528bf8ba659ccccd33a0ab34
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2024-06-18 23:34:46 +0600

    Create PROBLEM: 1070 - Six Odd Numbers.c

---
+++ PROBLEM: 1070 - Six Odd Numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+int a;
+scanf("%d",&a);
+if(a%2==0)
+{
+    a=a+1;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+}
+else
+{
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+    a=a+2;
+    printf("%d\n",a);
+}
+    return 0;
+}
