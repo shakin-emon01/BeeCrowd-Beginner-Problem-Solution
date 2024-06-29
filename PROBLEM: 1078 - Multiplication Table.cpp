8a65791bc4cfd82f3ec21ec56ee3bcb820c3fffd
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2024-06-29 10:18:50 +0600

    Create PROBLEM: 1078 - Multiplication Table.cpp

---
+++ PROBLEM: 1078 - Multiplication Table.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int N, i;
+ cin>>N;
+ for(i=1; i<=10; i++){
+int output = i*N;
+cout<<i<<" x "<<N<<" = "<<output<<endl;
+}
+    return 0;
+}
